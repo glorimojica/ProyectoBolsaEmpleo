48e666522429bd553f972507ab7ed22abac5620e
gmoji <gloriana.mojica.rojas@est.una.ac.cr>
2026-03-23 15:07:00 -0600

    commit

---
+++ src/main/java/cr/ac/una/proyectobolsaempleo/ProyectoBolsaEmpleoApplication.java
@@ -0,0 +1,13 @@
+package cr.ac.una.proyectobolsaempleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBolsaEmpleoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoBolsaEmpleoApplication.class, args);
+    }
+
+}
